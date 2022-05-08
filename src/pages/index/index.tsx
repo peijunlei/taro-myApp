be1b37f438249054d6a2ba2e68baab74cbf0b524
裴俊磊 <1594000695@qq.com>
2022-05-08 12:35:27 +0800

    feat(地图):
    add 高德地图

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,129 +1,175 @@
-import { Component } from 'react'
+import { Component, useEffect, useMemo, useState } from 'react'
 import { connect } from 'react-redux'
-import { View, Button, Text } from '@tarojs/components'
+import { View, Button, Text, ScrollView, Image, Input, Map, MapProps } from '@tarojs/components'
 
 import { add, minus, asyncAdd } from '../../actions/counter'
 
 import './index.less'
-import Taro from '@tarojs/taro'
+import Taro, { useDidShow } from '@tarojs/taro'
 import { getTaroSetting } from '../../utils'
 
-// #region 书写注意
-//
-// 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
-// 需要显示声明 connect 的参数类型并通过 interface 的方式指定 Taro.Component 子类的 props
-// 这样才能完成类型检查和 IDE 的自动提示
-// 使用函数模式则无此限制
-// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20796
-//
-// #endregion
+const amapFile = require('../../../libs/amap-wx.130')
+const WX_KEY = "a3a5045135be795588b12339c79090a4"
+const WEB_KEY = "d44a341bc63aee604518e1d2c15a6255"
+const Index = () => {
+  const [list, setList] = useState([])
+  const [list2, setList2] = useState([])
+  const [url, setUrl] = useState('')
+  const [address, setAddress] = useState()
+  const [keywords, setKeywords] = useState('新华汇')
+  const [location, setLocation] = useState(() => {
+    return Taro.getStorageSync("lngAndLat")
+  })
+  const markers = useMemo(() => {
+    return list.map((v: any) => {
+      const marker: MapProps.marker = {
+        id: parseInt(v.typecode),
+        longitude: typeof (v.location) === "string" ? (v.location as string).split(",")[0] as unknown as number : 0,
+        latitude: typeof (v.location) === "string" ? (v.location as string).split(",")[1] as unknown as number : 0,
+        iconPath: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbpic.136pic.com%2Fkimg%2F924%2F2015%2F10%2F25%2Ffa5e15480c2a1ee52ac606bf054fdc11.jpg&refer=http%3A%2F%2Fbpic.136pic.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1654569702&t=548d701d0aa8620909a538b473229064",
+        width: 20,
+        height: 20,
+      }
+      return marker
+    })
+  }, [list])
 
-type PageStateProps = {
-  counter: {
-    num: number
-  }
-}
+  const getTips = async () => {
+    if (!keywords.trim()) return Taro.showToast({ title: '不可为空', icon: 'none' })
 
-type PageDispatchProps = {
-  add: () => void
-  dec: () => void
-  asyncAdd: () => any
-}
+    const _keywords = keywords
+    console.log('_keywords', _keywords);
 
-type PageOwnProps = {}
-
-type PageState = {
-  a?: number
-}
-
-type IProps = PageStateProps & PageDispatchProps & PageOwnProps
-
-
-const mapStateToProps = ({ counter }) => ({
-  counter
-})
-const mapDispatchToProps = (dispatch) => ({
-  add() {
-    dispatch(add())
-  },
-  dec() {
-    dispatch(minus())
-  },
-  asyncAdd() {
-    dispatch(asyncAdd())
-  }
-})
-
-class Index extends Component<IProps, PageState> {
-  componentWillReceiveProps(nextProps) {
+    new amapFile.AMapWX({ key: WX_KEY }).getInputtips({
+      keywords: _keywords,
+      location,
+      success(data) {
+        console.log('getInputtips', data);
+        setList(data.tips)
+      },
+      fail(info) {
+        console.log(info);
+      }
+    })
   }
 
-  componentWillUnmount() { }
+  const getWebTips = async () => {
+    if (!keywords.trim()) return Taro.showToast({ title: '不可为空', icon: 'none' })
 
-  componentDidMount() {
-
-    this.openMap()
-
-    console.log(this.state?.a);
-
+    Taro.request({
+      url: "https://restapi.amap.com/v5/place/text",
+      data: {
+        key: WEB_KEY,
+        location,
+        keywords,
+        page_size: 30,
+      },
+      method: "GET",
+      header: { "content-type": "application/json" },
+      success: function (res) {
+        console.log('getWebKeywordsSearchV2', res.data);
+        setList(res.data.pois)
+      },
+      fail: function (err) {
+        console.log(err);
+      }
+    })
   }
-
-  componentDidHide() { }
-  openMap = () => {
+  /**获取经纬度 */
+  const getLocation = () => {
     getTaroSetting('userLocation').then(() => {
-      // 已经授权或还未授权，下面的代码也可以根据需求提取到公共文件中
       Taro.getLocation({
-        type: 'wgs84',
-        success: res => {
-          console.log(res);
+        type: "gcj02",
+        success(result) {
+          console.log(result);
+          const lngAndLatStr = result.longitude + "," + result.latitude
+          Taro.setStorageSync('lngAndLat', lngAndLatStr)
+          getRegeo(lngAndLatStr)
         },
-        fail: err => {
-          console.log(err);
-          Taro.showToast({
-            title: '频繁调用会增加电量损耗',
-            icon: 'none',
-            duration: 2000
-          })
-        }
+        fail(res) {
+          console.log(res);
+          Taro.showToast({ title: '失败', icon: 'none' })
+        },
       })
-    }).catch(err => {
-      console.log(err);
     })
   }
-  openMap2 = () => {
-    getTaroSetting('userInfo').then(() => {
-      // 已经授权或还未授权，下面的代码也可以根据需求提取到公共文件中
-     Taro.getUserInfo({
-       success:(res)=>{
-        console.log(res);
-        
-       },
-       fail(res) {
-         console.log(res);
-         
-       },
-     })
-    }).catch(err => {
-      console.log(err);
-    })
-  }
-  render() {
-    return (
-      <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <Button className='dec_btn' onClick={() => {
-          this.openMap()
-        }}>获取位置</Button>
-        <Button className='dec_btn' onClick={this.openMap2}>授权获取用户信息</Button>
 
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
-      </View>
-    )
+  /**获取当前位置详细信息 */
+  const getRegeo = (location) => {
+    const AMapWxFun = new amapFile.AMapWX({ key: WX_KEY })
+    AMapWxFun.getRegeo({
+      location,
+      iconPath: "./location.png",
+      success: function (res) {
+        setAddress(res[0])
+        Taro.showToast({ title: '获取位置成功', icon: 'none' })
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
   }
+  useDidShow(() => {
+    getLocation()
+  })
+  useEffect(() => {
+    getRegeo(location)
+  }, [location])
+  return (
+    <View className='map_container'>
+      <View>
+        当前位置:{address?.name }
+      </View>
+      <View className='search'>
+        <Input
+          className='ipt'
+          value={keywords}
+          onInput={(e) => setKeywords(e.detail.value)}
+        />
+        <View onClick={getTips} className="btn">搜索</View>
+      </View>
+
+      {/* <Button onClick={getWebTips}>获取提示web</Button> */}
+      {/* <Button onClick={getRegeo}>获取当前位置详细信息</Button> */}
+
+      <Map
+        style={{
+          width: "100%",
+          height: 400
+        }}
+        showCompass
+        showScale
+        className="map"
+        longitude={(location as string).split(",")[0] as unknown as number}
+        latitude={(location as string).split(",")[1] as unknown as number}
+        markers={markers}
+        onTap={(e) => {
+          const locationStr = e.detail.longitude + "," + e.detail.latitude
+          console.log('当前经纬度', locationStr);
+          setLocation(locationStr)
+        }}
+      ></Map>
+      {/* {
+        url && <Image src={url} />
+      } */}
+
+      <ScrollView>
+        {
+          list.map((v, i) =>
+            <View
+              style={{
+                fontSize: 10,
+                backgroundColor: "hotpink"
+              }}
+            >
+              {i + 1} - {v.district ? v.district + v.address + v.name : v.pname + v.cityname + v.adname + v.address + v.name}
+            </View>
+          )
+        }
+      </ScrollView>
+    </View>
+  )
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Index)
+export default Index;
 
